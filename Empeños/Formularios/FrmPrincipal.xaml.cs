6ebb0f67f24b2451e1cd488043a7f58b7ffcd336
Heiner Morales <hfmm99@hotmail.com>
2020-08-16 08:34:54 -0600

    Agregando lista de artículos en Grid de Empeños

--- Empeños/Formularios/FrmPrincipal.xaml.cs
+++ Empeños/Formularios/FrmPrincipal.xaml.cs
@@ -69,6 +69,7 @@ namespace Empeños.Formularios
             }
 
             var bd = new EmpeñosDataContext();
+
             switch (tabOpciones.SelectedIndex)
             {
                 case 0: //Empeños
